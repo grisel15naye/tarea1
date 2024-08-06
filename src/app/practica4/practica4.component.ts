7c6c6a0be146895a063d19f6e42273a9fc46e52a
thenightsuprime007 <carlos.flores.andres21@gmail.com>
2024-08-05 23:46:22 -0500

    tarea2

---
+++ src/app/practica4/practica4.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practica4',
+  standalone: true,
+  imports: [],
+  templateUrl: './practica4.component.html',
+  styleUrl: './practica4.component.css'
+})
+export class Practica4Component {
+
+}
