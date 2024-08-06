e17a1669e684f861e9dcc98d5e17fd6a236cb2f8
MarcoAguilarS <aguilarsantosmarco@gmail.com>
2024-08-05 23:08:42 -0500

    avance3

---
+++ src/app/practica2/practica2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practica2',
+  standalone: true,
+  imports: [],
+  templateUrl: './practica2.component.html',
+  styleUrl: './practica2.component.css'
+})
+export class Practica2Component {
+
+}
